a1c817212c35c745aaf77388bc0c3ad7279a96b6
Gustavo Machenski <machenski.gustavo@gmail.com>
2021-06-25 15:28:22 -0300

    att

---
+++ js/RDE15_JS_EX03.js
@@ -0,0 +1,15 @@
+/*Elabore um progama em que solicite ao usuario dois numeros n1 e n2. Realize a subtração dos dois valores. O progama não deve exibir numeros negativos*/
+
+var n1, n2, subtracao
+
+n1 = parseInt(prompt("Entre com um valor:"))
+n2 = parseInt(prompt("Entre com outro valor:"))
+
+if(n1>n2){
+    subtracao = n1 - n2
+    alert("O resultado é" + subtracao)
+}
+else{
+    subtracao = n2-n1
+    alert("O resultado é" + subtracao)
+}
---
+++ js/RDE15_JS_EX07.js
@@ -0,0 +1,8 @@
+function somar()
+{
+    var n1,n2,resultado
+    n1 =parseFloat(document.getElementById("n1").value)
+    n2 =parseFloat(document.getElementById("n2").value)
+    resultado=n1+n2
+    document.getElementById("resultado").innerHTML = `O resultado da soma é ${resultado}`
+}
